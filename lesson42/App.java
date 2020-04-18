789dd306d605e212319ce4e6363dfb3017d549e1
codershiyar <codershiyar@gmail.com>
2020-04-18 21:40:09 +0200

    42

---
+++ lesson42/App.java
@@ -0,0 +1,21 @@
+
+public class App extends Payment {
+
+  
+    public static void main(String[] args) {
+        String text = "Are you happy? \nYes\nNo ";
+          System.out.print(text);
+
+
+//   الرمز '\ لإضافة الرمز ' في مكان وضعها
+
+// الرمز "\ لإضافة الرمز " في مكان وضعها.
+
+// يضيف عدة مسافات في مكان وضعها  \t : الرمز          
+              
+//يجعل المحتوى الذي يأتي بعدها ينزل على سطر جديد. \n : الرمز  
+
+
+    }
+    
+}
