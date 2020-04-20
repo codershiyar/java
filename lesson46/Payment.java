fcbe0e4aadb194b55301fc5c3e55ca80500da74a
codershiyar <codershiyar@gmail.com>
2020-04-20 23:22:15 +0200

    46

---
+++ lesson46/Payment.java
@@ -0,0 +1,10 @@
+
+public class Payment {
+    Payment(){
+        System.out.println("طرق الدفع : ");
+        System.out.println("1: Visa Card");
+        System.out.println("2: Master Card");
+        System.out.println("3: PayPal");
+    }
+
+}
