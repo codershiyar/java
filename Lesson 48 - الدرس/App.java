1d34986afd856e77516dca8995cdcba50cb2d2c2
codershiyar <codershiyar@gmail.com>
2020-05-09 21:45:49 +0200

    Update

---
+++ Lesson 48 - الدرس/App.java
@@ -0,0 +1,27 @@
+
+public class App{
+  
+     App(){
+       
+     }
+     public static void main(String[] args) {
+  
+        int number1 = 10;
+        int number2 = 20;
+        System.out.println("العدد الأكبر يكون : " + Math.max(number1, number2));
+        System.out.println("العدد الأصغر يكون : " + Math.min(number1, number2));
+        System.out.println("الجذر تربيعي للعدد 36 يكون " + Math.sqrt(36));
+        int number3 = -15;
+        System.out.println("القيمة مطلقة للعدد - 15 يكون : " + Math.abs(number3));
+        
+        double random_number = Math.random();
+        System.out.println("العدد العشوائي يكون : " + random_number);
+     }
+     
+ }
+ 
+ 
+ 
+ 
+ 
+       
