5c12894194c4e5793286821ef60dc0dcd8a1a6f9
codershiyar <codershiyar@gmail.com>
2020-05-11 15:04:42 +0200

    update

---
+++ Lessson 50 - الدرس/Java/App.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class App extends App2 {
+
+   public static void main(String[] args) {
+ 
+   App.show_message();
+//    App2 app2 = new App2();
+   }
+
+}
+
---
+++ Lessson 50 - الدرس/Java/App2.java
@@ -0,0 +1,8 @@
+package Java;
+
+public abstract class App2 {
+    public static void show_message(){
+        
+        System.out.println("رسالة من ميتود داخل كلاس ابستركت");
+    }
+}
