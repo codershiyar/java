4db87c1fc2256dd569a2540534a9271b202a09cf
codershiyar <codershiyar@gmail.com>
2020-05-11 22:22:38 +0200

    51

---
+++ Lesson 51 - الدرس/Java/App.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class App extends App2 {
+   
+   @Override
+   public static void show_name(){
+      System.out.println("Name: Coder Shiyar");
+   }
+
+   public static void main(String[] args) {
+     App app = new App();
+     app.show_name();
+
+   }
+
+}
+
---
+++ Lesson 51 - الدرس/Java/App2.java
@@ -0,0 +1,10 @@
+package Java;
+
+public abstract class App2 {
+
+    public abstract static void show_name();
+
+    public static void show_message(){  
+        System.out.println("رسالة من ميتود داخل كلاس ابستركت");
+    }
+}
