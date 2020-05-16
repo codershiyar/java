175ea0c188e1fc5a8a7778cdf6667a00118176fb
codershiyar <codershiyar@gmail.com>
2020-05-16 15:09:57 +0200

    56

---
+++ Lesson 56 - الدرس/Java/Java/App.java
@@ -0,0 +1,10 @@
+package Java;
+
+class App {
+  public static void callAppHome(AppHome appHome){
+
+  }
+   public static void main(String[] args) {
+     App.callAppHome(new AppHome(" كل شي يعمل"));
+   }
+}
---
+++ Lesson 56 - الدرس/Java/Java/AppHome.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class AppHome{
+   AppHome(String message){
+       System.out.println("تم تنفيذ اوامر كونستركتور");
+       System.out.println(message);
+   } 
+}
